e5a61036b2fa6b6e366a7d34992d1f9b728732d7
Pujakokare <puja.sonur7@gmail.com>
2025-10-29 18:28:10 +0530

    Update authConfig.js

--- client/src/authConfig.js
+++ client/src/authConfig.js
@@ -1,13 +1,40 @@
 // client/src/authConfig.js
 export const msalConfig = {
   auth: {
-    clientId: "1e8189e9-e9a6-4b1c-b1ba-0a827bea564f",          // from Azure app
-    authority: "https://login.microsoftonline.com/ed5a80e6-e7e0-45f7-b0d0-a026d48e56f0",
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`,
     redirectUri: window.location.origin
   },
-  cache: { cacheLocation: "localStorage", storeAuthStateInCookie: false }
+  cache: {
+    cacheLocation: "sessionStorage",
+    storeAuthStateInCookie: false
+  }
 };
 
 export const loginRequest = {
-  scopes: ["openid", "profile", "email"],
+  scopes: ["openid", "profile", "email"]
 };
+
+
+
+
+
+
+
+
+
+
+
+// // client/src/authConfig.js
+// export const msalConfig = {
+//   auth: {
+//     clientId: "1e8189e9-e9a6-4b1c-b1ba-0a827bea564f",          // from Azure app
+//     authority: "https://login.microsoftonline.com/ed5a80e6-e7e0-45f7-b0d0-a026d48e56f0",
+//     redirectUri: window.location.origin
+//   },
+//   cache: { cacheLocation: "localStorage", storeAuthStateInCookie: false }
+// };
+
+// export const loginRequest = {
+//   scopes: ["openid", "profile", "email"],
+// };
