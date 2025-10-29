3645a18e726f2ee9adce31bcc84d9cbbfa0bee98
Pujakokare <puja.sonur7@gmail.com>
2025-10-29 11:05:14 +0530

    Update authConfig.js

--- client/src/authConfig.js
+++ client/src/authConfig.js
@@ -1,8 +1,8 @@
 // client/src/authConfig.js
 export const msalConfig = {
   auth: {
-    clientId: "YOUR_CLIENT_ID_HERE",          // from Azure app
-    authority: "https://login.microsoftonline.com/YOUR_TENANT_ID_HERE",
+    clientId: "1e8189e9-e9a6-4b1c-b1ba-0a827bea564f",          // from Azure app
+    authority: "https://login.microsoftonline.com/ed5a80e6-e7e0-45f7-b0d0-a026d48e56f0",
     redirectUri: window.location.origin
   },
   cache: { cacheLocation: "localStorage", storeAuthStateInCookie: false }
