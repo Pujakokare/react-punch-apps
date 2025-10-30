81278f64d4e53c1d3518adee2b1760ca77c54a79
Pujakokare <puja.sonur7@gmail.com>
2025-10-30 18:39:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(
     ],
    // methods: ['GET', 'POST'],
     methods: ["GET", "POST", "PUT", "DELETE"],
-   // allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   })
 );
