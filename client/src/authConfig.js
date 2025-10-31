53674a0e3c7e9723fbadc10ffc47ec075e907f0a
Pujakokare <puja.sonur7@gmail.com>
2025-10-31 15:58:06 +0530

    Update authConfig.js

--- client/src/authConfig.js
+++ client/src/authConfig.js
@@ -1,36 +1,36 @@
-// client/src/authConfig.js
-export const msalConfig = {
-  auth: {
-    clientId: process.env.REACT_APP_CLIENT_ID, // from Render env
-    authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`,
-    redirectUri: window.location.origin // ensure this matches the redirect URI you registered in Azure
-  },
-  cache: {
-    cacheLocation: "sessionStorage",
-    storeAuthStateInCookie: false
-  }
-};
-
-export const loginRequest = {
-  scopes: ["openid", "profile", "email", "api://1e8189e9-e9a6-4b1c-b1ba-0a827bea564f/access_as_user"]
-};
-
-// if you call MS Graph or other custom scopes, include them in scopes array when acquiring token
-
-
-
-
-
 // // client/src/authConfig.js
 // export const msalConfig = {
 //   auth: {
-//     clientId: "1e8189e9-e9a6-4b1c-b1ba-0a827bea564f",          // from Azure app
-//     authority: "https://login.microsoftonline.com/ed5a80e6-e7e0-45f7-b0d0-a026d48e56f0",
-//     redirectUri: window.location.origin
+//     clientId: process.env.REACT_APP_CLIENT_ID, // from Render env
+//     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}`,
+//     redirectUri: window.location.origin // ensure this matches the redirect URI you registered in Azure
 //   },
-//   cache: { cacheLocation: "localStorage", storeAuthStateInCookie: false }
+//   cache: {
+//     cacheLocation: "sessionStorage",
+//     storeAuthStateInCookie: false
+//   }
 // };
 
 // export const loginRequest = {
-//   scopes: ["openid", "profile", "email"],
+//   scopes: ["openid", "profile", "email", "api://1e8189e9-e9a6-4b1c-b1ba-0a827bea564f/access_as_user"]
 // };
+
+// // if you call MS Graph or other custom scopes, include them in scopes array when acquiring token
+
+
+
+
+
+// client/src/authConfig.js
+export const msalConfig = {
+  auth: {
+    clientId: "1e8189e9-e9a6-4b1c-b1ba-0a827bea564f",          // from Azure app
+    authority: "https://login.microsoftonline.com/ed5a80e6-e7e0-45f7-b0d0-a026d48e56f0",
+    redirectUri: window.location.origin
+  },
+  cache: { cacheLocation: "localStorage", storeAuthStateInCookie: false }
+};
+
+export const loginRequest = {
+  scopes: ["openid", "profile", "email"],
+};
