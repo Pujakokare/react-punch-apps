86e6c528852558648d0fdad17a5ef8410aea3fd2
Pujakokare <puja.sonur7@gmail.com>
2025-11-01 00:01:16 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -80,10 +80,10 @@ async function renderApp() {
 renderApp();
 
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
